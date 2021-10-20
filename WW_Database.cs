d3f80fa18abe0ade373c4a10b2ab6205c4f39847
White Wolf Game Studio <58172779+WhiteWolfStudio@users.noreply.github.com>
2021-10-20 20:57:57 +0200

    Update DB. Added Save to file

--- WW_Database.cs
+++ WW_Database.cs
@@ -1,81 +1,95 @@
 using UnityEngine;
+using System.IO;
 
 namespace WhiteWolf {
 
     public class WW_Database : MonoBehaviour {
 
         // Load FLOAT
-        public float LoadDataFloat( string name ) {
-            if ( PlayerPrefs.HasKey( name ) ) return PlayerPrefs.GetFloat( name );
+        public float LoadDataFloat(string name) {
+            if (PlayerPrefs.HasKey(name)) return PlayerPrefs.GetFloat(name);
             return -0.0f;
         }
 
         // Load INT
-        public int LoadDataInt( string name ) {
-            if ( PlayerPrefs.HasKey( name ) ) return PlayerPrefs.GetInt( name );
+        public int LoadDataInt(string name) {
+            if (PlayerPrefs.HasKey(name)) return PlayerPrefs.GetInt(name);
             return -0;
         }
 
         // Load STRING
-        public string LoadDataString( string name ) {
-            if ( PlayerPrefs.HasKey( name ) ) return PlayerPrefs.GetString( name );
+        public string LoadDataString(string name) {
+            if (PlayerPrefs.HasKey(name)) return PlayerPrefs.GetString(name);
             return null;
         }
 
         /*––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––*/
 
         // Save FLOAT
-        public void SaveDataFloat( string name, float value ) {
-            PlayerPrefs.SetFloat( name, value );
+        public void SaveDataFloat(string name, float value) {
+            PlayerPrefs.SetFloat(name, value);
             PlayerPrefs.Save();
         }
 
         // Save INT
-        public void SaveDataInt( string name, int value ) {
-            PlayerPrefs.SetInt( name, value );
+        public void SaveDataInt(string name, int value) {
+            PlayerPrefs.SetInt(name, value);
             PlayerPrefs.Save();
         }
 
         // Save STRING
-        public void SaveDataString( string name, string value ) {
-            PlayerPrefs.SetString( name, value );
+        public void SaveDataString(string name, string value) {
+            PlayerPrefs.SetString(name, value);
             PlayerPrefs.Save();
         }
 
         /*––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––*/
 
         // Check FLOAT
-        public bool CheakFloat( string name ){
-            if ( LoadDataFloat( name ) == -0f ) return false;
+        public bool CheakFloat(string name) {
+            if (LoadDataFloat(name) == -0f) return false;
             else return true;
         }
 
         // Check INT
-        public bool CheakInt( string name ){
-            if ( LoadDataInt( name ) == -0f ) return false;
+        public bool CheakInt(string name) {
+            if (LoadDataInt(name) == -0f) return false;
             else return true;
         }
 
         // Check STRING
-        public bool CheakString( string name ){
-            if ( LoadDataString( name ) == null ) return false;
+        public bool CheakString(string name) {
+            if (LoadDataString(name) == null) return false;
             else return true;
         }
 
         /*––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––*/
 
-        public void PlusFloat( string name, float n ) => SaveDataFloat( name, LoadDataFloat( name ) + n );
-        public void PlusInt( string name, int n ) => SaveDataInt( name, LoadDataInt( name ) + n );
+        public void PlusFloat(string name, float n) => SaveDataFloat(name, LoadDataFloat(name) + n);
+        public void PlusInt(string name, int n) => SaveDataInt(name, LoadDataInt(name) + n);
 
         /*––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––*/
 
-        public void MinusFloat( string name, float n ) => SaveDataFloat( name, LoadDataFloat( name ) - n );
-        public void MinusInt( string name, int n ) => SaveDataInt( name, LoadDataInt( name ) - n );
+        public void MinusFloat(string name, float n) => SaveDataFloat(name, LoadDataFloat(name) - n);
+        public void MinusInt(string name, int n) => SaveDataInt(name, LoadDataInt(name) - n);
 
         /*––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––*/
 
         // Reset INT / FLOAT / STRING
-        public void ResetData(){ PlayerPrefs.DeleteAll(); Debug.Log( "All data deleted!" ); }
+        public void ResetData() { PlayerPrefs.DeleteAll(); Debug.Log("All data deleted!"); }
+
+        /*––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––*/
+
+        public void SaveFile( string fileName, string data ){
+
+            string path = $"{Application.persistentDataPath}/{fileName}";
+
+            StreamWriter sw = File.AppendText( path );
+            sw.WriteLine( $"{data}" );
+
+        }
+
+        public string GetFilePath( string file ){ return $"{Application.persistentDataPath}/{file}"; }
 
     }
 
